c1cda88678e7ec6f7449b67fdedb0d0bd25a39b6
Adam Glasgall <glasgall@mit.edu>
2014-11-03 23:44:07 -0500

    hack for previewing puzzles from manage.py runserver

--- hunt/urls.py
+++ hunt/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import patterns, include, url
 
 
 urlpatterns = patterns('',
-    #
-    url(r'^all/$', 'hunt.views.puzzles_view'),
-    url(r'^(?P<puzzle>\w+)/$', 'hunt.views.puzzle_view'),
-    url(r'^meta/(?P<metapuzzle>\w+)/$', 'hunt.views.metapuzzle_view'),
+    url(r'^puzzle/(?P<puzzle>\w+)/$', 'hunt.views.puzzle_view'),
+    url(r'^round/(?P<round>\w+)/$', 'hunt.views.round_view'),
+    url(r'^$', 'hunt.views.top_view')
+#    url(r'^all/$', 'hunt.views.puzzles_view'),
+#    url(r'^meta/(?P<metapuzzle>\w+)/$', 'hunt.views.metapuzzle_view'),
 )
--- hunt/views.py
+++ hunt/views.py
@@ -1,18 +1,35 @@
 from django.shortcuts import render
 import actions
 import spoilr.models as spoilr_models
+from django.conf import settings
+import os
+import logging
+
+TEAMS_BASE = os.path.join(settings.HUNT_DATA_DIR, 'dev', 'docroot', 'teams')
 
 # type argument IS A BAD HACK
+# def puzzle_view(request, puzzle):
+#     return render(request, 'hunt/puzzle.tmpl', {'title': 'Example', 'puzzle_template': 'puzzle/' + puzzle + '/index.tmpl', 'puzzle': puzzle, 'team': request.META['REMOTE_USER'], 'type': 'puzzle'})
+
+# def metapuzzle_view(request, metapuzzle):
+#     return render(request, 'hunt/puzzle.tmpl', {'title': 'Example', 'puzzle_template': 'puzzle/' + metapuzzle + '/index.tmpl', 'puzzle': metapuzzle, 'team': request.META['REMOTE_USER'], 'type': 'meta'})
+
+# def puzzles_view(request):
+#     team = spoilr_models.Team.objects.get(username=request.META['REMOTE_USER'])
+#     puzzles = team.puzzles.all()
+#     rounds = team.rounds.all()
+
+#     return render(request, 'hunt/puzzles.tmpl', {'title': 'Puzzles for %s' % (team.name,),
+#                                                  'puzzles': puzzles, 'rounds': rounds})
+
 def puzzle_view(request, puzzle):
-    return render(request, 'hunt/puzzle.tmpl', {'title': 'Example', 'puzzle_template': 'puzzle/' + puzzle + '/index.tmpl', 'puzzle': puzzle, 'team': request.META['REMOTE_USER'], 'type': 'puzzle'})
+    puzzle_file = os.path.join(TEAMS_BASE, request.META['REMOTE_USER'], 'puzzle', puzzle, 'index.html')
+    return render(request, 'hunt/static_file.tmpl', {'path': puzzle_file})
 
-def metapuzzle_view(request, metapuzzle):
-    return render(request, 'hunt/puzzle.tmpl', {'title': 'Example', 'puzzle_template': 'puzzle/' + metapuzzle + '/index.tmpl', 'puzzle': metapuzzle, 'team': request.META['REMOTE_USER'], 'type': 'meta'})
+def round_view(request, round):
+    round_file = os.path.join(TEAMS_BASE, request.META['REMOTE_USER'], 'puzzle', round, 'index.html')
+    return render(request, 'hunt/static_file.tmpl', {'path':round_file})
 
-def puzzles_view(request):
-    team = spoilr_models.Team.objects.get(username=request.META['REMOTE_USER'])
-    puzzles = team.puzzles.all()
-    rounds = team.rounds.all()
-
-    return render(request, 'hunt/puzzles.tmpl', {'title': 'Puzzles for %s' % (team.name,),
-                                                 'puzzles': puzzles, 'rounds': rounds})
+def top_view(request):
+    top_file = os.path.join(TEAMS_BASE, request.META['REMOTE_USER'], 'index.html')
+    return render(request, 'hunt/static_file.tmpl', {'path': top_file})
